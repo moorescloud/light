fcf751679607d857ab31e00564a0317f4d3d8e54
3Easy <hello@3easy.org>
2012-10-05 12:25:16 +1000

    Papa Pesce’s Slideshow

--- javascripts/foundation/app.js
+++ javascripts/foundation/app.js
@@ -40,9 +40,31 @@
 		viewport.bind('load resize', fixHeight);
 	}
 	
+	// Hot Shots
+	$('#shots').orbit({
+		animation: 'fade', // fade, horizontal-slide, vertical-slide, horizontal-push
+		animationSpeed: 100, // how fast animtions are
+		timer: true, // true or false to have the timer
+		resetTimerOnClick: false, // true resets the timer instead of pausing slideshow progress
+		advanceSpeed: 20000, // if timer is enabled, time between transitions 
+		pauseOnHover: false, // if you hover pauses the slider
+		startClockOnMouseOut: false, // if clock should start on MouseOut
+		startClockOnMouseOutAfter: 1000, // how long after MouseOut should the timer start again
+		directionalNav: false, // manual advancing directional navs
+		captions: false, // do you want captions?
+		captionAnimation: 'fade', // fade, slideOpen, none
+		captionAnimationSpeed: 800, // if so how quickly should they animate in
+		bullets: false, // true or false to activate the bullet navigation
+		bulletThumbs: false, // thumbnails for the bullets
+		bulletThumbLocation: '', // location from this file where thumbs will be
+		afterSlideChange: function(){}, // empty function 
+		fluid: true // or set a aspect ratio for content slides (ex: '4x3')
+	});
+	
 		
 	// Fixed Nav
 	var productHeight = $('#product').outerHeight();
+	var navHeight = $('#product').outerHeight();
     $(window).scroll(function(){
         if ($(window).scrollTop() > $(window).height()){
 	        $('.nav').addClass('fixed').css('top','0');
